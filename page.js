c9f5bde7e9415a6c4b8be15b32a6aa2f1899c580
TeddyBird <ted850416@gmail.com>
2022-02-04 03:07:08 +0800

    update

--- page.js
+++ page.js
@@ -2,7 +2,7 @@ export default{
     props:["pages"],
     template:`<nav aria-label="Page navigation example">
         <ul class="pagination">
-            <li class="page-item" :class = "{disabled:!pages.has_pre}" @click = changePage("prev")>
+            <li class="page-item" :class = "{disabled:!pages.has_pre}" @click = "changePage('prev')">
                 <a class="page-link" href="#" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </a>
@@ -12,7 +12,7 @@ export default{
             v-for="(page,index) in pages.total_pages" :key = "page + 'page'">
                 <a class="page-link" href="#" @click = "$emit('get-page',page)">{{page}}</a>
             </li>
-            <li class="page-item" :class = "{disabled:!pages.has_next}" @click = changePage("next")>
+            <li class="page-item" :class = "{disabled:!pages.has_next}" @click = "changePage('next')">
                 <a class="page-link" href="#" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                 </a>
